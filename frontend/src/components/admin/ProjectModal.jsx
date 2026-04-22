8b98b01870f225a7232d97aff4ca06b77fee170c
kwesimain <opokulive@gmail.com>
2026-04-22 03:43:41 +0000

    edit projectmodal

--- frontend/src/components/admin/ProjectModal.jsx
+++ frontend/src/components/admin/ProjectModal.jsx
@@ -23,7 +23,7 @@ export default function ProjectModal({ isOpen, onClose, refreshData, editData })
   const techStack = [
     "python", "flask", "django", "react", "node.js", 
     "mongodb", "postgre", "tailwind", "javascript", 
-    "excel", "tableau", "figma", "google sheet", "html", "css"
+    "excel", "tableau", "figma", "google sheet", "html", "css", "git/github",
   ];
 
   // Close dropdown when clicking outside
